8d68a070b4346448ccb847d95f2637c0a19da3e9
Erik Mellum <erikmellum@eriks-mbp.lan>
2023-11-05 22:01:55 -0800

    Add supabase to fetch cards

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -5,11 +5,11 @@ export default function Card({
   img = 'http://placekitten.com/350/200',
 }) {
   return (
-    <div class="border-solid border-2 border-gray-600 p-6 bg-gray-100 rounded-lg shadow-md text-gray-900 hover:scale-105 transition-transform cursor-pointer grow">
-      <h1 class="text-3xl font-bold underline cursor-pointer">{title}</h1>
-      <h2 class="py-4">{subtitle}</h2>
+    <div className="border-solid border-2 border-gray-600 p-6 bg-gray-100 rounded-lg shadow-md text-gray-900 hover:scale-105 transition-transform cursor-pointer grow">
+      <h1 className="text-3xl font-bold underline cursor-pointer">{title}</h1>
+      <h2 className="py-4">{subtitle}</h2>
       <img src={img} />
-      <p class="py-4">{description}</p>
+      <p className="py-4">{description}</p>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,27 @@ import PageTitle from './components/PageTitle'
 import PageContent from './components/PageContent'
 import Card from './components/Card'
 
-export default function Home() {
+import { createClient } from '@supabase/supabase-js'
+const SUPABASE_URL = 'https://bzcrkwdrpftktwtohvsw.supabase.co'
+const SUPABASE_CARDS_TABLE = 'cards'
+// Create a single supabase client for interacting with your database
+const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_SECRET)
+
+export default async function Home() {
+  const { data: cards, error } = await supabase.from(SUPABASE_CARDS_TABLE).select()
+
+  console.log('cards', cards)
+
   return (
     <div>
       <Navbar />
       <div className="m-12">
         <PageTitle title="Home" />
         <PageContent />
-        <div class="flex gap-6 w-full my-6 flex-wrap">
-          <Card title="My First Card" subtitle="My first subtitle" description="my first description" />
-          <Card title="My second Card" subtitle="My second subtitle" description="my second description" />
-          <Card title="My third Card" subtitle="My third subtitle" description="my third description" />
+        <div className="flex gap-6 w-full my-6 flex-wrap">
+          {cards.map((card, idx) => (
+            <Card key={idx} title={card.title} subtitle={card.subtitle} description={card.description} />
+          ))}
         </div>
         <PageContent />
       </div>
